07f4577d09952f6b5ba62950af7c04debd34b9fb
yudenichaa <yudenichalexandr@yandex.ru>
2020-12-19 19:35:28 +0300

    Initial commit from Create Next App

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <html lang="en" />
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Template</title>
+        <link rel="icon" href="/images/favicon.ico" type="image/x-icon" />
+      </Head>
+    </>
+  );
+}
